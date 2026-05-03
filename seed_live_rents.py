281f54725bdf70bfe57b3f9e4e18e8d08fded9bc
PASCAL <pascalnjosh@gmail.com>
2026-05-03 21:25:19 +0300

    add seed script for live rents

---
+++ seed_live_rents.py
@@ -0,0 +1,25 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()
+
+from payments.models import Rent
+
+data = [
+    ("Oasis Business Center Tenant 2", 450000),
+    ("Oasis Business Center Tenant 1", 155000),
+    ("EASTWARD House 12 - BEDSITTER", 8000),
+    ("EASTWARD House 13 - ONE BEDROOM", 8000),
+    ("GULF House 9 - ONE BEDROOM", 7500),
+    ("MUTHAIGA House 1 - ONE BEDROOM", 7000),
+]
+
+for name, amount in data:
+    Rent.objects.get_or_create(
+        tenant_name=name,
+        amount=amount,
+        paid=False
+    )
+
+print("Seeded successfully")
