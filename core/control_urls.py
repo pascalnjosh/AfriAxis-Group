11cf28c7deba19288b73f89145f65ccc462f0df3
PASCAL <pascalnjosh@gmail.com>
2026-08-30 12:53:05 +0300

    Fix AfriAxis V7 control workspace routes

--- core/control_urls.py
+++ core/control_urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+
 from core.control_views import (
     accounts_workspace,
     audit_workspace,
     fairlane_management,
 )
+
 
 urlpatterns = [
     path(
@@ -11,10 +13,10 @@ urlpatterns = [
         accounts_workspace,
         name="v7_accounts_workspace",
     ),
+
     path(
         "audit-controls/",
         audit_workspace,
-    fairlane_management,
         name="v7_audit_workspace",
     ),
 
